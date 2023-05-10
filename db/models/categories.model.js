8828cb28947dd4e46f0cd8a8a3b9896c200bce8e
Maximilian Dent-Pooley <Max@Maximilians-MacBook-Pro-2.local>
2023-05-10 10:44:54 +0100

    deleted pesky console log

--- db/models/categories.model.js
+++ db/models/categories.model.js
@@ -2,7 +2,6 @@ const db = require('../connection');
 
 exports.fetchCategories = () => {
     return db.query('SELECT * FROM categories;').then((result) => {
-        console.log(result.rows, "in the model")
         return result.rows;
     })
 }
